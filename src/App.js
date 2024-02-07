5f313f7954cf466a02a7ad5ad8f1e6770426e401
evansdee <diegbee0@gmail.com>
2024-02-07 16:21:53 +0100

    new intake

--- src/App.js
+++ src/App.js
@@ -1,9 +1,101 @@
 import React from "react";
-import {nanoid} from 'nanoid'
+import { FaBars, FaCartPlus, FaSearch } from "react-icons/fa";
+import clothes from "./data";
+import { nanoid } from "nanoid";
 
+export default function App() {
+  const [clothesArr, setClothesArr] = React.useState(clothes);
+  const [selectedItem, setSelectedItem] = React.useState(clothesArr[0]);
+  return (
+    <div className="App">
+      <NavBar />
+      <p className="text-under-nav">
+        Home / Denim Pants / El Clasico Denim Pant
+      </p>
+      <DisplayItem selectedItem={selectedItem} />
+    </div>
+  );
+}
 
-export default function App(){
-  return <div className="App">
-    jdjdjjd
-  </div>
+function DisplayItem({ selectedItem }) {
+  // const [size,setSize] = React.useState(selectedItem.size)
+  const [selectedSize, setSelectedSizd] = React.useState(30);
+  const [count,setCount] = React.useState(1)
+
+  function prevCount(){
+    count > 1 && setCount(prev =>prev-1)
+  }
+  function nextCount(){
+    count < 12 && setCount(prev =>prev+1)
+  }
+
+  function onChangeSize(value) {
+    setSelectedSizd(value);
+  }
+
+  return (
+    <div className="displayItem">
+      <img src={selectedItem.img} alt={selectedItem.alt} />
+      <div className="displayText">
+        <h1>{selectedItem.clotheName}</h1>
+        <p id="one">${selectedItem.price}.00</p>
+        <p id="two">Tax included. Shipping calculated at checkout</p>
+        <p id="three">Size: {selectedSize}</p>
+
+        <div className="sizes">
+          {selectedItem.size.map((ele) => {
+            return (
+              <Size
+                key={ele.sizeNum}
+                selectedSize={selectedSize}
+                ele={ele}
+                onChangeSize={onChangeSize}
+              />
+            );
+          })}
+        </div>
+
+        <div className="color">Color: {selectedItem.color} <br />
+        <span></span>
+        </div>
+        <div className="quantity">
+          <p>
+            Quantity: 
+          </p>
+          <p>
+            <span onClick={prevCount}>-</span>
+            <span>{count}</span>
+            <span onClick={nextCount}>+</span>
+          </p>
+        </div>
+        <Button>Add to Cart</Button>
+        <Button>Buy it now</Button>
+      </div>
+    </div>
+  );
+}
+
+function Size({ ele, onChangeSize, selectedSize }) {
+  return (
+    <div
+      className={`size ${ele.sizeNum === selectedSize && "activeColor"}`}
+      style={ele.isAvailble ? {} : { textDecoration: "line-through" }}
+      onClick={() => onChangeSize(ele.sizeNum)}
+    >
+      {ele.sizeNum}
+    </div>
+  );
+}
+function NavBar() {
+  return (
+    <nav>
+      <FaBars className="icon-size" />
+      <img src="./img/logo.avif" alt="" />
+      <FaCartPlus className="icon-size" />
+    </nav>
+  );
+}
+
+function Button({ children, onClick }) {
+  return <button onClick={onClick}>{children}</button>;
 }
---
+++ src/data.js
@@ -0,0 +1,123 @@
+import { nanoid } from "nanoid";
+
+const clothes = [
+  {
+    id:nanoid(),
+    clotheName: "El Clasico Denim Pant",
+    price: 175,
+    img: "./img/pants.webp",
+    alt: "https://ash-luxe.com/cdn/shop/products/Ashluxetemplate_3370x.png?v=1698408091",
+    color: "blue",
+    size: [
+      {
+        id:nanoid(),
+        sizeNum: 30,
+        isAvailble: true,
+      },
+      {
+        id:nanoid(),
+
+        sizeNum: 32,
+        isAvailble: true,
+      },
+      {
+        id:nanoid(),
+
+        sizeNum: 34,
+        isAvailble: false,
+      },
+      {
+        id:nanoid(),
+
+        sizeNum: 36,
+        isAvailble: true,
+      },
+    ],
+  },
+  {
+    clotheName: "ASHLUXE Logo Blank High neck T-shirt White",
+    price: 98,
+    img: "./img/neck.webp",
+    alt: "https://cdn.shopify.com/s/files/1/0066/2326/4828/products/AshluxeBlankWhite_4efe05c5-08a4-46da-a5c8-753eee1cfc24_720x.png?v=1698408105",
+    color: "white",
+    size: [
+      {
+        sizeNum: "S",
+        isAvailble: false,
+      },
+      {
+        sizeNum: "M",
+        isAvailble: false,
+      },
+      {
+        sizeNum: "L",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "XL",
+        isAvailble: true,
+      },
+    ],
+  },
+  {
+    clotheName: "Men's Denim Trucker Jean - Black",
+    price: 520,
+    img: "./img/",
+    alt: "https://cdn.shopify.com/s/files/1/0066/2326/4828/products/Ashluxe_Men_s_Denim_Trucker_Jean-Black_19605e50-87f8-4d9b-bd8c-e13c5b235502_720x.png?v=1698408072",
+    color: "Black",
+    size: [
+      {
+        sizeNum: "S",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "M",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "L",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "XL",
+        isAvailble: true,
+      },
+    ],
+  },
+  {
+    clotheName: "Ashluxe Denim Shorts Washed Blue",
+    price: 340,
+    img: "./img/shorts.webp",
+    alt: "https://cdn.shopify.com/s/files/1/0066/2326/4828/products/WashDenimBlue_0d5ccfe8-2cce-4328-b9ec-667412cb8f6a_720x.png?v=1698408102",
+    color: "blue",
+    size: [
+      {
+        sizeNum: "S",
+        isAvailble: false,
+      },
+      {
+        sizeNum: "M",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "L",
+        isAvailble: true,
+      },
+      {
+        sizeNum: "XL",
+        isAvailble: true,
+      },
+    ],
+  },
+  {
+    clotheName: "Ashluxe Mirro Glasses - Red",
+    price: 120,
+    img: "./img/glass.webp",
+    alt: "https://cdn.shopify.com/s/files/1/0066/2326/4828/products/11_d307e1a7-212f-43a1-9342-d2624d5f2a0d_720x.png?v=1698408068",
+    color: "red",
+    size: "OS"
+  }
+];
+
+
+export default clothes
